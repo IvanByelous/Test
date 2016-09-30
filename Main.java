4ea95d8090870e0e8e60d922413473b6a1a97eec
IvanByelous <Byelous.cloud@gmail.com>
2016-09-30 19:06:43 +0300

    compiled and bug fixed

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-import java.io.*
+import java.io.*;
 
 public class Main{
 
 	public static void main(String []args){
 		
 		System.out.println("Hello world,bitches!");
-		File file = new File("VanyaNePidar.txt",true);
+		File file = new File("VanyaNePidar.txt");
 	}	
 }
